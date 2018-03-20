4a0444bb2f2699a6bf12b3ff1285ee15ea99722c
gusiushub <zolowkin.nikita@yandex.ru>
2018-03-20 20:18:24 +0300

    new change

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -75,37 +75,29 @@ BlogAsset::register($this);
         <!-- Links -->
         <section>
             <ul class="links">
-                <li>
-                    <a href="#">
-                        <h3>Lorem ipsum</h3>
-                        <p>Feugiat tempus veroeros dolor</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <h3>Dolor sit amet</h3>
-                        <p>Sed vitae justo condimentum</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <h3>Feugiat veroeros</h3>
-                        <p>Phasellus sed ultricies mi congue</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <h3>Etiam sed consequat</h3>
-                        <p>Porta lectus amet ultricies</p>
-                    </a>
-                </li>
+                <li><a href="/web/site/index">Главная</a></li>
+                <li><a href="/web/site/about">О нас</a></li>
+                <li><a href="/web/site/snake">Змейка</a></li>
+                <?php if (Yii::$app->user->isGuest): ?>
+                    <li><a href="/web/site/signup">Регистрация</a></li>
+                    <li><a href="/web/site/login">Авторизация</a></li>
+                <?php else: ?>
+                    <?= Html::beginForm(['/web/site/logout'],'post')
+                    .Html::submitButton(
+                        'Logout (' . Yii::$app->user->identity->email . ')',
+                        ['class' => 'btn btn-link logout']
+                    )
+                    . Html::endForm() ?>
+                <?php endif; ?>
             </ul>
         </section>
 
         <!-- Actions -->
         <section>
             <ul class="actions vertical">
+                <?php if (Yii::$app->user->isGuest): ?>
                 <li><a href="/web/site/login" class="button big fit">Log In</a></li>
+                <?php endif; ?>
             </ul>
         </section>
 
